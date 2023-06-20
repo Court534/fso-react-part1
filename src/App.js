6ce3ca76244ccc4faf5f545f6c19cfa13bea1a30
Courtney Stow <courtney-stow@hotmail.co.uk>
2023-06-20 15:32:50 +0100

    Messing around with props

--- src/App.js
+++ src/App.js
@@ -1,17 +1,23 @@
+const Hello = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <p>Hello {props.name}, you are {props.age} years old</p>
+    </div>
+  )
+}
+
 const App = () => {
-  const now = new Date();
-  const a = 10;
-  const b = 20;
-  console.log(now, a + b);
+  const name = 'Courtney'
+  const age = 29
 
   return (
     <div>
-      <p>Hello there!</p>
-      <p>
-        {a} plus {b} is {a + b}
-      </p>
+      <h1>Greetings</h1>
+      <Hello name='Isabelle' age={20 + 7}/>
+      <Hello name={name} age={age} />
     </div>
-  );
+  )
 };
 
 export default App;
